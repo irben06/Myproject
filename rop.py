1a7ad317c97c4bd801606187eb7d9da6eac8b8c4
irben06 <Ajlex401@gmail.com>
2023-11-16 16:02:50 +0600

    Make some changes

--- rop.py
+++ rop.py
@@ -1,1 +1,14 @@
-a
+def red(a,b):
+    if a > b:
+        return ("a больше")
+    elif a < b:
+        return ("b больше")
+    else:
+        return ("Они равны")
+    
+
+print(red(7, 7))
+        
+
+    
+    
